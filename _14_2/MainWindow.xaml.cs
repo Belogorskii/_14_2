dc834d3230a27f320676b843ff2b143c22ac9471
Kirill Miller <belogorski2000@gmail.com>
2022-08-26 18:11:38 +0300

    hrhehrh

---
+++ _14_2/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace _14_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProdName = "Бананы",
+                ProdPrice = 70,
+                ProdImage = "Data/Бананы.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Кокос",
+                ProdPrice = 120,
+                ProdImage = "Data/Кокос.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Ананас",
+                ProdPrice = 260,
+                ProdImage = "Data/Ананас.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Пылесос",
+                ProdPrice = 7000,
+                ProdImage = "Data/Пылесос.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Утюг",
+                ProdPrice = 3500,
+                ProdImage = "Data/Утюг.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Стиральная машинка",
+                ProdPrice = 35000,
+                ProdImage = "Data/Стиральная_машина.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            listBox.ItemsSource = products;
+        }
+    }
+}
